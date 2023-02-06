101b417f2fa5aa470f371636652e1669b2b108d6
Mike <v3mikeangel@gmail.com>
2023-02-06 16:20:34 -0600

    my first api 3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const { request, response } = require('express')
 
 app.use(cors())
-
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
